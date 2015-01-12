e7bdb0b65161dff8eba7e7ef0c1a7cfba5863992
thist <st.thiem@gmail.com>
2015-01-12 18:15:32 +0000

    solution assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## ----------------------------------------------------------------------------------
+## Special matrix class which also stores its inverse value
+## ----------------------------------------------------------------------------------
+## 
+## To create cacheMatrix object: cache_matrix <- makeCacheMatrix(matrix)
+## To compute inverse and store it: cacheSolve(cache_matrix)
+## To access matrix and inverse: cache_matrix$get() and cache_matrix$get_inverse()
+## ----------------------------------------------------------------------------------
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##This function creates a special matrix object that can cache its inverse.
+makeCacheMatrix <- function(matrix_data = matrix()) {
+    inverse <- NULL
+    
+    ## set function to store matrix data
+    set <- function(y) {
+        matrix_data <<- y
+        inverse <<- NULL
+    }
+    
+    ## return matrix data
+    get <- function() matrix_data
+    
+    ## compute inverse of matrix and store in inverse
+    set_inverse <- function(solve) inverse <<- solve
+    
+    ## return inverse of matrix
+    get_inverse <- function() inverse
+    
+    ## list all available functions
+    list(set = set, get = get, set_inverse = set_inverse, get_inverse = get_inverse)
 }
 
+## This function computes the inverse of the special matrix returned by makeCacheMatrix. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cacheSolve retrieves the inverse from the cache.
+cacheSolve <- function(cmatrix, ...) {
+    ## get inverse from matrix if already computed
+    inverse <- cmatrix$get_inverse()
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
+    
+    ## otherwise compute inverse and store it
+    data <- cmatrix$get()
+    inverse <- solve(data, ...)
+    cmatrix$set_inverse(inverse)
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of cmatrix    
+    inverse
 }
